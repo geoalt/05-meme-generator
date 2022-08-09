04b9181f39c3ac73960210165e025dfc8af0a451
Diogo Martins <me.martinsfe@gmail.com>
2022-08-09 13:42:06 -0300

    feat: complete item 1

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 /* Projeto iniciado */
+// DOM
+const paraInput = document.querySelector('#text-input');
+const memeTextPreview = document.querySelector('#meme-text');
+
+const printText = () => {
+  memeTextPreview.innerText = paraInput.value;
+};
+
+
+// Eventos
+paraInput.addEventListener('keyup', printText);
