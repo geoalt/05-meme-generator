fdd5de148e1f37c99ee3e18fb00244f2218b5ba0
Diogo Martins <me.martinsfe@gmail.com>
2022-08-10 02:01:24 -0300

    feat: complete item 6, and add images to memes

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 /* Projeto iniciado */
 // DOM
+const showMeme = document.querySelector('#meme-image-container');
 const paraInput = document.querySelector('#text-input');
 const memeTextPreview = document.querySelector('#meme-text');
 const imageInput = document.querySelector('#meme-insert');
+const fireButton = document.querySelector('#fire');
+const waterButton = document.querySelector('#water');
+const earthButton = document.querySelector('#earth');
 
 // Funcao que faz o preview do texto inserido no input de tipo texto
 const printText = () => {
@@ -22,6 +26,25 @@ imageInput.addEventListener("change", function () {
   reader.readAsDataURL(this.files[0]);
 });
 
+// Muda borda para 1 (fire)
+const setFire = () => {
+  const fireBorder = getComputedStyle(fireButton).border;
+  showMeme.style.border = fireBorder;
+};
+
+const setWater = () => {
+  const waterBorder = getComputedStyle(waterButton).border;
+  showMeme.style.border = waterBorder;
+};
+
+const setEarth = () => {
+  const earthBorder = getComputedStyle(earthButton).border;
+  showMeme.style.border = earthBorder;
+};
+
 
 // Eventos
 paraInput.addEventListener('keyup', printText);
+fireButton.addEventListener('click', setFire);
+waterButton.addEventListener('click', setWater);
+earthButton.addEventListener('click', setEarth);
