d881311137e6e6c67ae11c8b34aa5c86bf876ce1
Diogo Martins <me.martinsfe@gmail.com>
2022-08-09 22:12:04 -0300

    feat: complete items 2 e 3

--- script.js
+++ script.js
@@ -2,10 +2,25 @@
 // DOM
 const paraInput = document.querySelector('#text-input');
 const memeTextPreview = document.querySelector('#meme-text');
+const imageInput = document.querySelector('#meme-insert');
 
+// Funcao que faz o preview do texto inserido no input de tipo texto
 const printText = () => {
   memeTextPreview.innerText = paraInput.value;
 };
+
+
+// Funcao que faz o preview da imagem a ser usada na criacao do meme
+
+
+imageInput.addEventListener("change", function () {
+  const reader = new FileReader();
+  reader.addEventListener("load", () => {
+    const uploaded_image = reader.result;
+    document.querySelector("#meme-image").style.backgroundImage = `url(${uploaded_image})`;
+  });
+  reader.readAsDataURL(this.files[0]);
+});
 
 
 // Eventos
