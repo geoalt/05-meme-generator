af9167d417ea58ef95bdf530ddaa0740246d72fa
Diogo Martins <me.martinsfe@gmail.com>
2022-08-10 02:25:30 -0300

    feat: complete item 7, but not

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ const imageInput = document.querySelector('#meme-insert');
 const fireButton = document.querySelector('#fire');
 const waterButton = document.querySelector('#water');
 const earthButton = document.querySelector('#earth');
+const memePreset = document.querySelectorAll('.meme');
 
 // Funcao que faz o preview do texto inserido no input de tipo texto
 const printText = () => {
@@ -32,19 +33,31 @@ const setFire = () => {
   showMeme.style.border = fireBorder;
 };
 
+// Muda borda para 2 (water)
 const setWater = () => {
   const waterBorder = getComputedStyle(waterButton).border;
   showMeme.style.border = waterBorder;
 };
 
+// Muda borda para 3 (earth)
 const setEarth = () => {
   const earthBorder = getComputedStyle(earthButton).border;
   showMeme.style.border = earthBorder;
 };
 
+// Muda imagem do meme 'pre-pronto'
+const changePic = (e) => {
+  const memeImagePreview = document.querySelector('#meme-image');
+  const getSource = e.target.src
+  memeImagePreview.style.backgroundImage = `url(${getSource}`;
+};
 
 // Eventos
 paraInput.addEventListener('keyup', printText);
 fireButton.addEventListener('click', setFire);
 waterButton.addEventListener('click', setWater);
 earthButton.addEventListener('click', setEarth);
+
+for (let item of memePreset) {
+  item.addEventListener('click', changePic);
+}
